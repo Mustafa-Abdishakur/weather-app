dd60a593291883d91678fd322c7746ee468945ce
mustafa abdishakur <mx_1200@hotmail.com>
2020-05-17 01:01:15 +0400

    add

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
-async function test(){
+async function test(city){
     try{
-        let forecast = await fetch('https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=AbuDhabi&units=metric&appid=28ce1addf74f9faaa95257b4c4d31b9f');
+        let forecast = await fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=28ce1addf74f9faaa95257b4c4d31b9f`);
         let json = await forecast.json();
         console.log(json);
     }catch(err){
@@ -8,4 +8,5 @@ async function test(){
     }
 }
 
-test();
+//test('دبي');
+
